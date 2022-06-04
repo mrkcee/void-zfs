0d8ec0cb916487ac14f65d0ef80d28ed6344f721
mrkcee <markz_a@yahoo.com>
2022-06-04 18:57:21 +0800

    Converted chroot cleanup script to fish

---
+++ 99-chroot_cleanup.fish
@@ -0,0 +1,11 @@
+#!/bin/fish
+
+# Umount all partitions
+echo 'Unmount partitions used during chroot...'
+umount /mnt/efi
+umount -l /mnt/{dev,proc,sys}
+zfs umount -a
+
+# Export zpool
+print 'Exporting zpool'
+zpool export zroot
