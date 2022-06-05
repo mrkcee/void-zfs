f857ec8362920a18849bf8d2461ca90f8fd478ed
mrkcee <markz_a@yahoo.com>
2022-06-05 20:20:19 +0800

    Added chmod to pipewire script

--- 04-pipewire.fish
+++ 04-pipewire.fish
@@ -9,15 +9,18 @@ pipewire
 install_packages $packages
 
 ## Disable autospawn in pulseaudio
-echo "autospawn=no" >> /etc/pulse/client.conf
+echo "autospawn= no " >> /etc/pulse/client.conf
 
 ## Create pipewire script in /opt
+set -l pipewire_script /opt/pipewire/pipewire.sh
 mkdir -p /opt/pipewire
 echo "\
 #!/bin/sh
 pipewire &
 pipewire-pulse &
-" > /opt/pipewire/pipewire.sh 
+" > $pipewire_script
+
+chmod +x $pipewire_script
 
 ## Create desktop shortcut
 echo "\
@@ -30,4 +33,4 @@ Exec=/opt/pipewire/pipewire.sh
 " > /usr/share/applications/pipewire.desktop
 
 print_success "Pipewire has been installed and configured successfully."
-
+echo "Note: Configure pipewire to autostart when your desktop manager loads."
