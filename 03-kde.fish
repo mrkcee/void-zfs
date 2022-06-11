418704a8f9da9d890ffb72fee014fa98a02a8009
mrkcee <markz_a@yahoo.com>
2022-06-11 13:34:44 +0800

    Added KDE script

---
+++ 03-kde.fish
@@ -0,0 +1,24 @@
+#!/bin/fish
+
+check_if_root
+
+echo "Checking for installed nvidia drivers..."
+if not xbps-query nvidia
+  print_error "NVIDIA package not installed."
+  exit 1
+end
+
+echo "Installing and configuring KDE..."
+set -l packages \
+xorg-minimal \
+kde5 \
+kde5-baseapps
+
+install_packages $packages
+
+echo "Enabling elogin and SDDM service..."
+ln -s /etc/sv/elogind /var/service
+ln -s /etc/sv/sddm /var/service
+
+print_success "KDE installation is successfull."
+
