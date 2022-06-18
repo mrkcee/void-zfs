3afe054d57d4bd070ad81c0bc3a13cf5558f5ee1
mrkcee <markz_a@yahoo.com>
2022-06-18 13:51:00 +0800

    Code clean up

--- 04-pipewire.fish
+++ 04-pipewire.fish
@@ -8,29 +8,5 @@ pipewire
 
 install_packages $packages
 
-## Disable autospawn in pulseaudio
-echo "autospawn= no " >> /etc/pulse/client.conf
-
-## Create pipewire script in /opt
-set -l pipewire_script /opt/pipewire/pipewire.sh
-mkdir -p /opt/pipewire
-echo "\
-#!/bin/sh
-pipewire &
-pipewire-pulse &
-" > $pipewire_script
-
-chmod +x $pipewire_script
-
-## Create desktop shortcut
-echo "\
-[Desktop Entry]
-Name=Pipewire
-Type=Application
-Terminal=false
-Comment=Autostart Pipewire
-Exec=/opt/pipewire/pipewire.sh
-" > /usr/share/applications/pipewire.desktop
-
 print_success "Pipewire has been installed and configured successfully."
 echo "Note: Configure pipewire to autostart when your desktop manager loads."
